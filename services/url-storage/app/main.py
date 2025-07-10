6ccc7346871d9d3c63ba8a5937e22743328d4fdd
santoshvarmaaddala <santoshvarma2166@gmail.com>
2025-07-10 11:33:09 +0530

    now shortner can communicate with storage service

--- services/url-storage/app/main.py
+++ services/url-storage/app/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+import redis
+
 
 app = FastAPI()
+r = redis.Redis(host="redis", port=6379, decode_responses=True)
 
 @app.get("/health")
 def health_check():
     return {"status": "url-storage is healthy"}
+
+@app.post("/store")
+def store_url(code: str = Query(...), original_url: str = Query(...)):
+    r.set(code, original_url)
+    return {"message": "stored", "code": code}
+
+@app.get("/retrieve/{code}")
+def retrieve_url(code: str):
+    url = r.get(code)
+    if url:
+        return {"url": url}
+    return {"error": "not found"}
+
